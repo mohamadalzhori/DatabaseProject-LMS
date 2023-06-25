d6d20da7d66db42665480dfbb8c4fc09f602524c
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-06-25 15:43:23 +0300

    kalaoun change

--- server/routes/Students.js
+++ server/routes/Students.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {Students} = require("../models");
-const bcrypt = require('bcrypt');
+const { Students } = require("../models");
+const bcrypt = require("bcrypt");
+//test kalaoun
 
-const {sign} = require ("jsonwebtoken");
+const { sign } = require("jsonwebtoken");
 
-router.get('/',async (req,res)=>{
-    res.json(await Students.findAll());
+router.get("/", async (req, res) => {
+  res.json(await Students.findAll());
 });
 
-router.put('/', async (req, res) => {
+router.put("/", async (req, res) => {
   const { username, points } = req.body;
-  
+
   try {
     // Find the student with the provided username in the Students table
     const student = await Students.findOne({ where: { username } });
@@ -21,56 +22,55 @@ router.put('/', async (req, res) => {
       student.points = points;
       await student.save(); // Save the changes to the database
 
-      res.status(200).json({ message: 'Points updated successfully.' });
+      res.status(200).json({ message: "Points updated successfully." });
     } else {
-      res.status(404).json({ error: 'Student not found.' });
+      res.status(404).json({ error: "Student not found." });
     }
   } catch (error) {
-    console.error('Error updating points:', error);
-    res.status(500).json({ error: 'An internal server error occurred.' });
+    console.error("Error updating points:", error);
+    res.status(500).json({ error: "An internal server error occurred." });
   }
 });
 
-
-router.post('/', async (req, res) => {
-    const { username, password, points, grade_id } = req.body;
-    bcrypt.hash(password, 10).then((hash) => {
-      Students.create({
-        username,
-        password: hash,
-        points,
-        grade_id,
-      }).then(() => {
-        res.json('success');
-      });
+router.post("/", async (req, res) => {
+  const { username, password, points, grade_id } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Students.create({
+      username,
+      password: hash,
+      points,
+      grade_id,
+    }).then(() => {
+      res.json("success");
     });
   });
-  
+});
 
-router.post('/login', async (req, res) => {
-    const { username, password} = req.body;
-  
-    const user = await Students.findOne({ where: { username: username } });
-  
-    if (!user) {
-      return res.json({ error: "User doesn't exist" });
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Students.findOne({ where: { username: username } });
+
+  if (!user) {
+    return res.json({ error: "User doesn't exist" });
+  }
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) {
+      return res.json({ error: "Wrong Username and Password Combination" });
     }
-  
-    bcrypt.compare(password, user.password).then((match) => {
-      if (!match) {
-        return res.json({ error: "Wrong Username and Password Combination" });
-      }
-      const accessToken = sign({username: user.username, id: user.id}, "importantsecret");
-      const studentData = {
-        accessToken: accessToken,
-        username: user.username, // Assuming you have a 'name' property in the 'Students' model
-        grade: user.grade // Assuming you have a 'grade' property in the 'Students' model
-      };
-  
-      res.json(studentData);
-    });
-  });
-  
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    const studentData = {
+      accessToken: accessToken,
+      username: user.username, // Assuming you have a 'name' property in the 'Students' model
+      grade: user.grade, // Assuming you have a 'grade' property in the 'Students' model
+    };
 
+    res.json(studentData);
+  });
+});
 
 module.exports = router;
