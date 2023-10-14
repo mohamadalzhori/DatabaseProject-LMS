28fd8e0d5ab37d0d3e09bd5aa4a16f565860f3ba
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-10-14 23:05:06 +0300

    Home Page and Student Dash

--- client/src/Components/OnlineCourses.js
+++ client/src/Components/OnlineCourses.js
@@ -16,37 +16,42 @@ const OnlineCourses = ({ setActiveSubject }) => {
       hoverCover: "./images/courses/online/o1.1.png",
       courseName: "Arabic",
       course: "25 Courses",
+      subject_id: "1",
     },
     {
       cover: "./images/courses/online/o2.png",
       hoverCover: "./images/courses/online/o2.1.png",
       courseName: "English",
       course: "25 Courses",
+      subject_id: "2",
     },
     {
       cover: "./images/courses/online/o3.png",
       hoverCover: "./images/courses/online/o3.1.png",
       courseName: "Math",
       course: "10 Courses",
+      subject_id: "3",
     },
     {
       cover: "./images/courses/online/o4.png",
       hoverCover: "./images/courses/online/o4.1.png",
       courseName: "Sciences",
       course: "15 Courses",
+      subject_id: "4",
     },
     {
       cover: "./images/courses/online/o5.png",
       hoverCover: "./images/courses/online/o5.1.png",
       courseName: "Islamic Culture",
       course: "30 Courses",
+      subject_id: "5",
     },
-    {
-      cover: "./images/courses/online/o5.png",
-      hoverCover: "./images/courses/online/o5.1.png",
-      courseName: "shi shaghle krmel l style",
-      course: "30 Courses",
-    },
+    // {
+    //   cover: "./images/courses/online/o5.png",
+    //   hoverCover: "./images/courses/online/o5.1.png",
+    //   courseName: "shi shaghle krmel l style",
+    //   course: "30 Courses",
+    // },
   ];
   return (
     <>
@@ -58,7 +63,7 @@ const OnlineCourses = ({ setActiveSubject }) => {
           </div>
           <div className="content grid3">
             {online.map((val) => (
-              <div className="box" onClick={() => subject(val.courseName)}>
+              <div className="box" onClick={() => subject(val.subject_id)}>
                 <div className="img">
                   <img src={val.cover} />
                   <img src={val.hoverCover} alt="" className="show" />
